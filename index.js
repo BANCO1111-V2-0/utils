1c32333e81045499a15d2b3a25f62afa4ec6196c
ajiopay <rudivalsilvadesouza01@gmail.com>
2024-04-05 14:25:55 -0300

    Atualizar .env e index.js

--- index.js
+++ index.js
@@ -15,8 +15,24 @@ const headers = {
     'x-delbank-api-key': process.env.DELBANK_API_KEY
 };
 
+// Função para obter o saldo atual da conta do cliente
+function obterSaldo() {
+    return axios.get('https://api.delbank.com.br/baas/api/v1/balances', { headers })
+        .then(response => {
+            return response.data.amountAvailable; // Alterado para acessar amountAvailable em vez de balance
+        })
+        .catch(error => {
+            console.error('Erro ao obter saldo:', error);
+            throw error;
+        });
+}
+
 axios.get('https://api.delbank.com.br/baas/api/v2/transactions', { params, headers })
-    .then(response => {
+    .then(async response => {
+        // Obtendo o saldo atual da conta do cliente
+        const saldoAtual = await obterSaldo();
+        console.log("Saldo Atual da Conta do Cliente:", saldoAtual);
+
         const entradas = [];
         const saidas = [];
         let valorTotalEntradas = 0;
@@ -28,7 +44,7 @@ axios.get('https://api.delbank.com.br/baas/api/v2/transactions', { params, heade
                 status: transacao.status,
                 tipo: transacao.type.name,
                 nome: transacao.type.description,
-                descricao: transacao.description,
+                description: transacao.description,
                 isCredit: transacao.type.isCredit
             };
 
@@ -51,6 +67,9 @@ axios.get('https://api.delbank.com.br/baas/api/v2/transactions', { params, heade
         console.log(saidas);
         console.log("Valor Total de Saídas: " + valorTotalSaidas.toFixed(2));
         console.log("Valor Total de Entradas: " + valorTotalEntradas.toFixed(2)); // Corrigido para mostrar o valor total de entradas
+
+        // Log do saldo atual da conta do cliente
+        console.log("Saldo Atual da Conta do Cliente:", saldoAtual);
     })
     .catch(error => {
         console.error('Erro ao fazer a solicitação:', error);
