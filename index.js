a7a3df86197487623ed84954be3f735a9785a3d7
ajiopay <rudivalsilvadesouza01@gmail.com>
2024-04-07 14:03:26 -0300

    Atualizar  index.js

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const startDate = process.env.START_DATE;
-const endDate = process.env.END_DATE;
-
-const params = {
-    startDate,
-    endDate,
-    page: 1,
-    limit: 99999
+const obterSaldo = async () => {
+    try {
+        const headers = {
+            'x-delbank-api-key': process.env.DELBANK_API_KEY
+        };
+        const response = await axios.get('https://api.delbank.com.br/baas/api/v1/balances', { headers });
+        return response.data.amountAvailable;
+    } catch (error) {
+        console.error('Erro ao obter saldo:', error);
+        throw error;
+    }
 };
 
-const headers = {
-    'x-delbank-api-key': process.env.DELBANK_API_KEY
-};
+const main = async () => {
+    try {
+        const startDate = process.env.START_DATE;
+        const endDate = process.env.END_DATE;
+        const params = {
+            startDate,
+            endDate,
+            page: 1,
+            limit: 99999
+        };
 
-// Função para obter o saldo atual da conta do cliente
-function obterSaldo() {
-    return axios.get('https://api.delbank.com.br/baas/api/v1/balances', { headers })
-        .then(response => {
-            return response.data.amountAvailable; // Alterado para acessar amountAvailable em vez de balance
-        })
-        .catch(error => {
-            console.error('Erro ao obter saldo:', error);
-            throw error;
-        });
-}
+        const headers = {
+            'x-delbank-api-key': process.env.DELBANK_API_KEY
+        };
 
-axios.get('https://api.delbank.com.br/baas/api/v2/transactions', { params, headers })
-    .then(async response => {
-        // Obtendo o saldo atual da conta do cliente
+        const response = await axios.get('https://api.delbank.com.br/baas/api/v2/transactions', { params, headers });
+
         const saldoAtual = await obterSaldo();
         console.log("Saldo Atual da Conta do Cliente:", saldoAtual);
 
@@ -38,24 +39,31 @@ axios.get('https://api.delbank.com.br/baas/api/v2/transactions', { params, heade
         let valorTotalEntradas = 0;
         let valorTotalSaidas = 0;
 
+        const contagem = { credito: 0, debito: 0 };
+
         response.data.forEach(transacao => {
             const transacaoFormatada = {
                 valor: transacao.amount,
-                status: transacao.status,
                 tipo: transacao.type.name,
-                nome: transacao.type.description,
-                description: transacao.description,
+                nome: transacao.type.description || transacao.type.name, // Use type.name se description for undefined
+                description: transacao.description || transacao.type.description, // Use type.description se description for undefined
                 isCredit: transacao.type.isCredit
             };
-
+        
+            console.log("Descrição da Transação:", transacaoFormatada.description);
+            console.log("É um crédito?", transacaoFormatada.isCredit);
+        
             if (transacaoFormatada.isCredit) {
                 entradas.push(transacaoFormatada);
                 valorTotalEntradas += transacao.amount;
+                contagem.credito++;
             } else {
                 saidas.push(transacaoFormatada);
                 valorTotalSaidas += transacao.amount;
+                contagem.debito++;
             }
         });
+        
 
         console.log("Entradas:");
         console.log(entradas);
@@ -66,11 +74,15 @@ axios.get('https://api.delbank.com.br/baas/api/v2/transactions', { params, heade
         console.log("Saídas:");
         console.log(saidas);
         console.log("Valor Total de Saídas: " + valorTotalSaidas.toFixed(2));
-        console.log("Valor Total de Entradas: " + valorTotalEntradas.toFixed(2)); // Corrigido para mostrar o valor total de entradas
+        console.log("Valor Total de Entradas: " + valorTotalEntradas.toFixed(2));
 
-        // Log do saldo atual da conta do cliente
         console.log("Saldo Atual da Conta do Cliente:", saldoAtual);
-    })
-    .catch(error => {
+
+        console.log("Contagem de Transações:");
+        console.log(contagem);
+    } catch (error) {
         console.error('Erro ao fazer a solicitação:', error);
-    });
+    }
+};
+
+main();
